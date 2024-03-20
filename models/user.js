600d2ed0f1ad9b45198742b3126d82a20d776895
masoudvrz <masoud.varzi@yahoo.com>
2024-03-20 16:20:23 +0330

    feat : define user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phonenumber: { type: String, required: true },
+    role: {
+      type: String,
+      required: true,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
