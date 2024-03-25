51ccddd5d8a176061650110d2521f750e67bb7b1
masoudvrz <masoud.varzi@yahoo.com>
2024-03-25 17:30:01 +0330

    feat : added internal server error middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path")
 
 const authRouter = require("./routes/v1/auth");
+const userRouter = require("./routes/v1/user");
 
 const app = express();
 //The express.static() function is a built-in middleware function in Express.
@@ -19,5 +20,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user" , userRouter);
+
+
+app.use((error , req , res , next)=> {
+    res.status(error.status || 500).json({messg: error.message || "SERVER INTERNAL ERROR"})
+  })
 
 module.exports = app;
