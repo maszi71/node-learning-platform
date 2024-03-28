b1178fcd62c36a159fe555d3d504548d380489f7
masoudvrz <masoud.varzi@yahoo.com>
2024-03-28 17:15:20 +0330

    feat : implemented update role and delete user routes

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -1,7 +1,9 @@
 const BlockedUser = require("../../models/blocked-user");
 const User = require("../../models/user");
+const { ADMIN, USER } = require('../../constants/role');
+const { isValidObjectId } = require("mongoose");
 
-const blockUser = async (req, res) => {
+const blockUser = async (req, res, next) => {
   const id = req.params.id;
   const user = await User.findOne({ _id: id });
   if (user) {
@@ -16,6 +18,68 @@ const blockUser = async (req, res) => {
   }
 };
 
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    const filteredUsers = users.map((user) => {
+      const convertedUser = user.toObject();
+      delete convertedUser.password;
+      return convertedUser;
+    });
+    return res.status(200).json(filteredUsers);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const removeUser = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const isValidId = isValidObjectId(userId);
+    if (!isValidId) {
+      return res.status(409).json({ message: "Id is not Valid" });
+    }
+    const deletedUser = await User.findOneAndDelete({ _id: userId });
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+    return res.status(200).json({ message: "user is deleted successfully" });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const updateRole = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const { role } = req.body;
+    const isValidId = isValidObjectId(userId);
+    if (!isValidId) {
+      return res.status(409).json({ message: "Id is not Valid" });
+    }
+    if (role === ADMIN || role === USER) {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: userId },
+        { $set: { role: role } }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User Not Found" });
+      }
+      return res
+        .status(200)
+        .json({ message: "user role is updated successfully" });
+    } else {
+      res.status(404).json({ message: "Role is not Valid" });
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
   blockUser,
+  getAllUsers,
+  removeUser,
+  updateRole,
 };
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const {blockUser} = require("../../controllers/v1/userController");
+const {blockUser , getAllUsers , removeUser , updateRole} = require("../../controllers/v1/userController");
 const isAdmin = require("../../middlewares/isAdminMiddleware");
 const hasAuthToken = require("../../middlewares/authMiddleware");
 const router = express.Router();
 
 router.put("/block-user/:id" , hasAuthToken ,isAdmin ,blockUser );
+router.get("/" , hasAuthToken ,isAdmin , getAllUsers);
+router.delete("/:id" , hasAuthToken , isAdmin , removeUser);
+router.put("/role/:id" , hasAuthToken , isAdmin, updateRole);
+
 
 module.exports = router;
