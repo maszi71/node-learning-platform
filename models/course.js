5f383f0476edd8b7c0e73a656b14c626bb1db76c
masoudvrz <masoud.varzi@yahoo.com>
2024-04-13 12:23:42 +0330

    feat: defined course model

---
+++ models/course.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    cover: { type: String, required: true },
+    address: { type: String, required: true },
+    support: { type: String, required: true },
+    price: { type: Number, required: true },
+    stutus: { type: String, required: true },
+    discount: { type: Number, required: true },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    teacher: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+courseSchema.virtual("sessions", {
+  ref: "Session",
+  localField: "_id",
+  foreignField: "course",
+});
+
+courseSchema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "course",
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+module.exports = Course;
