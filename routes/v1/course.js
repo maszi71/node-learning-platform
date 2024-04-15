19b22f896c327fce679cc2618676770d4ccca820
masoudvrz <masoud.varzi@yahoo.com>
2024-04-15 19:15:59 +0330

    feat: added session route

--- routes/v1/course.js
+++ routes/v1/course.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const isAdmin = require("../../middlewares/isAdminMiddleware");
 const hasAuthToken = require("../../middlewares/authMiddleware");
-const { createNewCourse  } = require("../../controllers/v1/courseController");
-const uploader = require("../../utils/uploader")
+const {
+  createNewCourse,
+  createNewSession,
+} = require("../../controllers/v1/courseController");
+const uploader = require("../../utils/uploader");
 
 const router = express.Router();
-router.route("/").post(hasAuthToken , isAdmin , uploader.single("cover"), createNewCourse);
+router
+  .route("/")
+  .post(
+    hasAuthToken,
+    isAdmin,
+    uploader("image", "covers", 1024 * 1024 * 2).single("cover"),
+    createNewCourse
+  );
+router
+  .route("/:id/session")
+  .post(
+    hasAuthToken,
+    isAdmin,
+    uploader("video", "videos", 1024 * 1024 * 10).single("video"),
+    createNewSession
+  );
 
 module.exports = router;
