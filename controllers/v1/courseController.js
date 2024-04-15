e9dc1b80ea66e9be15a989f589fa39a796af172d
masoudvrz <masoud.varzi@yahoo.com>
2024-04-15 19:16:21 +0330

    feat: implemented session controller

--- controllers/v1/courseController.js
+++ controllers/v1/courseController.js
@@ -1,5 +1,9 @@
 const Course = require("../../models/course");
+const Session = require("../../models/session");
 const courseValidator = require("../../validators/course");
+const { isValidObjectId } = require("mongoose");
+const sessionValidator = require("../../validators/session");
+
 
 const createNewCourse = async (req, res,next) => {
   try {
@@ -43,6 +47,27 @@ const createNewCourse = async (req, res,next) => {
   }
 };
 
+const createNewSession = async (req , res , next) => {
+  const {title , time , free} = req.body;
+  const courseId = req.params.id;
+  if (!isValidObjectId(courseId)) {
+    return res.status(409).json({ message: "Id is not Valid" });
+  }
+  const isValidSession = sessionValidator(req.body);
+  if (isValidSession !== true) {
+    return res.status(429).json(isValidSession);
+  }
+  const addedSession = await Session.create({
+    title,
+    time,
+    free,
+    video : req.file.filename,
+    course : courseId
+  });
+  return res.status(201).json(addedSession)
+}
+
 module.exports = {
   createNewCourse,
+  createNewSession
 };
